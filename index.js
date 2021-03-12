061a1bd80ccb3160932caa7efb0dda28c2fc17fc
Amio <amio.cn@gmail.com>
2021-03-12 14:14:47 +0800

    feat: support buffer payload

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ function send(url, payload) {
   const http = url.startsWith('http://') ? require('http') : require('https')
 
   if (payload === undefined) {
-    http.get(url)
-  } else {
-    http.request(url, { method: 'POST' }).write(payload)
+    return http.get(url)
+  }
+
+  if (typeof payload === 'string'){
+    return http.request(url, { method: 'POST' }).write(payload)
+  }
+
+  if (typeof payload === 'object'){
+    const { encode, text } = payload
+    const bufferPayload = Buffer.from(text, encode)
+    return http.request(url, { method: 'POST' }).write(bufferPayload)
   }
 }
 
@@ -24,11 +32,9 @@ function sendBeacon(url, payload) {
     throw new Error(`Invalid url (${url})`)
   }
 
-  // TODO: support buffer type payload
-
   const invokeString = payload === undefined
     ? `;send('${url}')`
-    : `;send('${url}', '${payload.replace(/'/g, '\\\'')}')`
+    : `;send('${url}', ${stringifyPayload(payload)})`
 
   const evalString = send.toString() + invokeString
 
@@ -38,6 +44,26 @@ function sendBeacon(url, payload) {
   })
 
   subprocess.unref()
+}
+
+/**
+ * @param {string|Buffer} payload
+ * @returns {string} 'xxxx' | '{ "encode": "base64", }'
+ */
+function stringifyPayload (payload) {
+  if (typeof payload === 'string') {
+    return `'${payload.replace(/'/g, '\\\'')}'`
+  }
+
+  if (Buffer.isBuffer(payload)) {
+    const encode = 'base64'
+    return JSON.stringify({
+      text: payload.toString(encode),
+      encode,
+    })
+  }
+
+  throw new TypeError('payload should be string or buffer')
 }
 
 module.exports = sendBeacon
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const sendBeacon = require('./')
 
-sendBeacon('https://timeout.now.sh/3001')
+sendBeacon('https://timeout.now.sh/3001', Buffer.from('adf'))
 
 // takes 3 seconds to finish
 // require('https').get('https://timeout.now.sh/3002')
