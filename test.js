8a298566e45df1ddd8c470611114c29b2170c2a0
Amio <amio.cn@gmail.com>
2021-03-14 15:19:04 +0800

    test: update test for simple get

--- test.js
+++ test.js
@@ -1,6 +1,28 @@
-const sendBeacon = require('./')
+const assert = require('assert')
+const { createServer } = require('comets')
+const sendBeacon = require('.')
 
-sendBeacon('https://timeout.now.sh/3001', Buffer.from('adf'))
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+async function testSimpleGet () {
+  let accessedUrl
+
+  const requestHandler = async (req, res) => {
+    accessedUrl = req.url
+    await sleep(1000)
+    res.end()
+  }
+
+  const address = await createServer(requestHandler)
+  sendBeacon(address + '/eiyo')
+
+  await sleep(100)
+  assert.strictEqual(accessedUrl, '/eiyo')
+}
+
+testSimpleGet()
+
+// sendBeacon('https://timeout.now.sh/3001', Buffer.from('adf'))
 
 // takes 3 seconds to finish
 // require('https').get('https://timeout.now.sh/3002')
